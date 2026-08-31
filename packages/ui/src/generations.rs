79479638e8032ce4a5c54acdf2ddaa3ceffed443
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-08-31 19:14:42 +0700

    add struct generations

--- packages/ui/src/generations.rs
+++ packages/ui/src/generations.rs
@@ -1,33 +1,34 @@
 use dioxus::prelude::*;
 
-const GEN_CSS: Asset = asset!("/assets/styling/generation.css");
+const GEN_CSS: Asset = asset!("/assets/styling/hero.css");
 
 #[component]
 pub fn GenerationMenu() -> Element {
-    // struct generations {
-    //     gen: String,
-    //     nixos_version: String,
-    //     kernel: String,
-    //     Built_on: String
-    // }
+    struct generations {
+        gen: String,
+        nixos_version: String,
+        kernel: String,
+        Built_on: String
+    }
 
     rsx! {
         document::Link { rel: "stylesheet", href: GEN_CSS }
 
         div {
-            div { 
-                ul { id: "generation",
-                    li { a {  "NixOs (Generation 11 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
-                    li { a {  "NixOs (Generation 10 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
-                    li { a {  "NixOs (Generation 9 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
-                    li { a {  "NixOs (Generation 8 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
-                    li { a {  "NixOs (Generation 7 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
-                    li { a {  "NixOs (Generation 6 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
-                    li { a {  "NixOs (Generation 5 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
-                    li { a {  "NixOs (Generation 4 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
-                    li { a {  "NixOs (Generation 3 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
-                    li { a {  "NixOs (Generation 2 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
-                    li { a {  "NixOs (Generation 1 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"  } }
+            div { id: "links",
+                ul { 
+                    li {
+                        a { 
+                            href: "https://msalmanrafadhlih.github.io/tquilla.is-a.dev/generation_2/",
+                            "NixOs (Generation 2 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"
+                        }
+                    }
+                    li {
+                        a { 
+                            href: "https://msalmanrafadhlih.github.io/tquilla.is-a.dev/generation_1/",
+                            "NixOs (Generation 1 NixOS 26.11.2979.47c003416129, Linux Kernel 5.15.97, Built on 2026-03-07)"
+                        }
+                    }
                 }
                 p {  "Reboot Into Firmware Interface" }
             }
